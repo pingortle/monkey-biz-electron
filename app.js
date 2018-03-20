9f95e4bb1553a2d2347cc6df45cd50b776f7c62d
Kaleb Lape <kaleb.lape@metova.com>
2018-03-20 16:41:59 -0500

    Run monkey biz integration test. 🎉

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const udp = require('dgram')
+
 const choo = require('choo')
 const html = require('choo/html')
+const { MonkeyBusinessTester, OpenPipe } = require('monkey-biz-node')
 
 const app = choo()
 
@@ -8,5 +11,14 @@ app.route('/', main)
 app.mount('#app')
 
 function main() {
-  return html`<div>hello choo</div>`
+  return html`
+    <div>
+      <button onclick=${runTest}>Run Test</button>
+    </div>
+  `
+
+  function runTest() {
+    const tester = new MonkeyBusinessTester(new OpenPipe(udp.createSocket('udp4')))
+    tester.run().catch(error => console.error(error))
+  }
 }
