22be078aee2562f2bfd35fa0cbf8bfe1b6b433d4
Kaleb Lape <kaleb.lape@metova.com>
2018-03-22 06:46:20 -0500

    Disable button for in flight operations.

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ const app = choo()
 
 app.route('/', main)
 
+app.use((state, emitter) => {
+  state.disabled = ''
+
+  emitter.on('running', () => {
+    console.log('running')
+    state.disabled = 'disabled'
+    emitter.emit('render')
+  })
+
+  emitter.on('done', () => {
+    console.log('done')
+    state.disabled = ''
+    emitter.emit('render')
+  })
+})
+
 app.mount('#app')
 
-function main() {
+function main(state, emit) {
   return html`
     <div>
-      <button onclick=${runTest}>Run Test</button>
+      <button ${state.disabled} onclick=${runTest}>Run Test</button>
     </div>
   `
 
-  function runTest() {
+  async function runTest() {
     const tester = new MonkeyBusinessTester(new OpenPipe(udp.createSocket('udp4')))
-    tester.run().catch(error => console.error(error))
+    emit('running')
+    await tester.run().catch(error => console.error(error))
+    emit('done')
   }
 }
